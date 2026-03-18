dcde308cd4b8efc331bc5cfc49be39f2762a19ad
sist5 <sist5@sist-5>
2026-03-18 17:48:57 +0900

    260318

---
+++ Chapter06_객체지향프로그램(변수초기화-활용)/src/문제.java
@@ -0,0 +1,95 @@
+
+class Movie_
+{
+	String title;
+	String poster;
+	String date;
+	String genre;
+	static String country;
+	String grade;
+	String runningTime;
+	double 평점;
+	String 박스오피스;
+	String 시놉시스;
+}
+
+class News
+{
+	String title;
+	String image;
+	static String 기자;
+	String 내용;
+	String date;
+	static String 신문사;
+}
+
+class Blog
+{
+	String image;
+	String title;
+	String word;
+}
+
+class 맛집
+{
+	String name;
+	double 평점;
+	String adress;
+	String phone;
+	String 음식종류;
+	String 가격대;
+	String 주차;
+	String time;
+	String[] menu;
+	String[] menuPrice;
+}
+
+class recipe
+{
+	String video;
+	String title;
+	String 작성자;
+	int 별점;
+	int 조회수;
+}
+
+class Music
+{
+	int 순위;
+	String 변동;
+	String title;
+	String image;
+	String artist;
+	String Album;
+	int 좋아요;
+}
+
+class Mart
+{
+	String image;
+	String title;
+	String 인하율;
+	String price;
+}
+
+class 연극
+{
+	String title;
+	boolean 공연중여부;
+	String image;
+	String genre;
+	String date;
+	String 장소;
+	String[] actor;
+	String grade;
+	String runningTime;
+}
+
+public class 문제 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
